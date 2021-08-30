99a91bded016ae23453de55afe0cc38dcbb2bf64
danielle-zn <84985016+danielle-zn@users.noreply.github.com>
2021-08-30 14:51:01 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from "./logo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header">
+          <img
+            src={logo}
+            alt="danielle's logo"
+            className="img-fluid danielle-logo"
+          />
+        </header>
+      </div>
+    </div>
+  );
+}
+
+export default App;
